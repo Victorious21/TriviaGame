3cfabf01ad4d78eff3f3e132d0ed8e2555dc02ad
Zaharia <0274237@students.dadeschools.net>
2019-04-16 12:01:19 -0400

    started the questions, finished html; started on js

---
+++ main.js
@@ -0,0 +1,23 @@
+var panel = $("#quiz-area");
+
+var countStartNumber = 30;
+
+// Question Set
+var questions = [
+    {
+        question: "Who was Hirohito?",
+        answers: ["Japanese Axis Ruler","Philipino Dictator","Irish boss", "A Nazi Government leader"],
+        correctAnswer: "Japanese Axis Ruler",
+        image: "C:\Users\0274237\Desktop\Hangman Completed\assets\download.jpg"
+    },
+
+    {
+        question: "What was the Russian government named under during WW2?",
+        answers: ["Nazi","Republica Italiano","USSR", "MURICA"],
+        correctAnswer: "USSR",
+        image: "C:\Users\0274237\Desktop\Hangman Completed\assets\download.png"
+    },
+
+
+
+]
