628163e1285d7f1a675a026b818f0993d6e7eca5
Zaharia <0274237@students.dadeschools.net>
2019-04-18 11:52:18 -0400

    wokred on a little more

--- main.js
+++ main.js
@@ -19,5 +19,49 @@ var questions = [
     },
 
 
-
 ]
+
+// Variable to hold our setInterval
+var timer;
+
+var game = {
+
+    questions: questions,
+    currentQuestion: 0,
+    counter: countStartNumber,
+    correct: 0,
+    incorrect: 0,
+
+    countdown: function() {
+        game.counter--;
+        $("#counter-number").html(game.counter);
+        if(game.counter === 0) {
+            console.log("YOU ARE TOO SLOW, GAME OVER!!!");
+            game.timeUp();
+        }
+
+    },
+
+    loadQuestion: function() {
+        timer = setInterval(game.countdown, 1000);
+        panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
+        for (var i = 0; i < questions[this.currentQuestion].answers.length; i++)
+        {
+            panel.append(" <button class='answer-button' id= 'button' data-name=' " + 
+           questions[this.currentQuestion].answers[i] + ">'" +
+           questions[this.currentQuestion].answers[i] + "</button>" );
+        }
+    },
+
+    nextQuestion: function() {
+        game.counter = countStartNumber;
+        $("#counter-number").html(game.counter);
+        game.currentQuestion++;
+        game.loadQuestion();
+    }
+
+    timeUp: function() {
+        
+    }
+
+}
