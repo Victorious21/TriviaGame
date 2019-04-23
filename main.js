4f572518d3c4dbb2d476ac9b6ece6cdf8b3c8b8a
Zaharia <0274237@students.dadeschools.net>
2019-04-23 11:35:25 -0400

    worked on js

--- main.js
+++ main.js
@@ -58,10 +58,40 @@ var game = {
         $("#counter-number").html(game.counter);
         game.currentQuestion++;
         game.loadQuestion();
-    }
+    },
 
     timeUp: function() {
-        
-    }
+
+        clearInterval(timer);
+
+        $("#counter-number").html(game.counter);
+
+        panel.html("<h2>OUT OF TIME!</h2>");
+        panel.append("<h3>The Correct Answer Was: " + questions[this.currentQuestion].correctAnswer);
+        panel.append("<img src='" + questions[this.currentQuestion].image);
+
+        if (game.currentQuestion === questions.length -1)
+        {
+            setTimeout(game.results, 3 * 1000);
+        }
+        else{
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
+
+    },
+
+    results: function() {
+
+        clearInterval(timer);
+
+        panel.html("<h2>All done, here are your results!!!!!!!!!!!</h2>");
+        $("counter-number").html(game.counter);
+
+        panel.append("<h3>Correct Answers: " + game.correct + "</h3>");
+        panel.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
+        panel.append("<h3>Unanswered: " + (questions.length - (game.correct + game.incorrect)));
+        panel.append("<br><button id= 'start-over'>Start Over?")
+
+    },
 
 }
